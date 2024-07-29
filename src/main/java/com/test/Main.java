bd06eb0bbcff9cc882d12fddbcbbb15abf281478
sumin <dltnfals@gmail.com>
2024-07-29 15:51:19 +0900

    ok

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello worl!");
+        System.out.println("Have a good day!, Bye2");
+    }
+}
